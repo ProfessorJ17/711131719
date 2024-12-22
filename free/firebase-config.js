c1231fa04bcbffb05ccf9764190468d5cd0df9e8
ProfessorJ17 <162835589+ProfessorJ17@users.noreply.github.com>
2024-12-22 16:29:50 -0600

    Create firebase-config.js

---
+++ free/firebase-config.js
@@ -0,0 +1,12 @@
+// firebase-config.js
+const firebaseConfig = {
+  apiKey: "AIzaSyB8Ura2PrHeMnIXryULLcMJ0thONSO7_mE",
+  authDomain: "freemoney-4e2e4.firebaseapp.com",
+  projectId: "freemoney-4e2e4",
+  storageBucket: "freemoney-4e2e4.appspot.com",
+  messagingSenderId: "13864207359",
+  appId: "1:13864207359:web:d51b2b0c91de4c91b406d75",
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
