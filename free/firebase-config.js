490adb65f4dc0153a9c5c7599578d440f4f1e655
ProfessorJ17 <162835589+ProfessorJ17@users.noreply.github.com>
2024-12-22 17:44:01 -0600

    Update firebase-config.js

--- free/firebase-config.js
+++ free/firebase-config.js
@@ -1,12 +1,9 @@
 // firebase-config.js
 export const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,  // Use GitHub secret for API key
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN, // Use GitHub secret for Auth domain
-  projectId: process.env.FIREBASE_PROJECT_ID,  // Use GitHub secret for project ID
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET, // Use GitHub secret for storage bucket
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,  // Use GitHub secret for messaging sender ID
-  appId: process.env.FIREBASE_APP_ID, // Use GitHub secret for app ID
+  apiKey: process.env.FIREBASE_API_KEY,  // GitHub secret for API key
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN, // GitHub secret for Auth domain
+  projectId: process.env.FIREBASE_PROJECT_ID,  // GitHub secret for project ID
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET, // GitHub secret for storage bucket
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,  // GitHub secret for messaging sender ID
+  appId: process.env.FIREBASE_APP_ID, // GitHub secret for app ID
 };
-
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
